edf436bebbd44536f7869914a6eb96f8b150ada2
JSebastian20 <62495587+JSebastian20@users.noreply.github.com>
2020-03-24 00:42:49 +1030

    split p and img

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,17 @@
 import React from "react";
+import Avatar from "./Avatar";
+import P from "./P";
 
 function Card(props) {
   return (
     <div className="card">
       <div className="top">
         <h2 className="name">{props.name}</h2>
-        <img className="circle-img" src={props.img} alt="avatar_img" />
+        <Avatar img={props.img} />
       </div>
       <div className="bottom">
-        <p className="info">{props.tel}</p>
-        <p className="info">{props.email}</p>
+        <P data={props.tel} />
+        <P data={props.email} />
       </div>
     </div>
   );
---
+++ src/components/P.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+function P(props) {
+  return <p className="info">{props.data}</p>;
+}
+export default P;
